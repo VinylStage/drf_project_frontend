c453f9d1fbd330558adce96ddb64f8e74fc215b7
VinylStage <mcpe9869@gmail.com>
2023-04-21 20:06:44 +0900

    0421 add  signup, login, mock, logout

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("Loaded");
+
+window.onload = () => {
+  const payload = localStorage.getItem("payload");
+  const payload_parse = JSON.parse(payload);
+  console.log(payload_parse.email);
+
+  const intro = document.getElementById("intro");
+  intro.innerText = payload_parse.email;
+};
